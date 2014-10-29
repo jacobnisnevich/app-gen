6f63927736eb2beb15d9682004ce3c229073e8a6
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-29 16:44:28 -0700

    now prints to screen

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 
 request({ url: url, json: true }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      console.log(body) // Print the json response
+      response.send(body) // Print the json response
     }
 });
 

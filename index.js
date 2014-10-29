8701b33992756cce8c740a1ce5d6d05a1eccb4a4
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-29 16:06:28 -0700

    installed request

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
 var request     = require('request');
 var apiKey      = '54f83928fbeec5b60aa70187744ca1ed'; // your API key
 var packageName = 'com.whatsapp';     // package Name, e.g. com.whatsapp for WhatsApp
 
 var url = 'http://api.playstoreapi.com/v1.1/apps/' + packageName + '?key=' + apiKey;
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 request({ url: url, json: true }, function (error, response, body) {
     if (!error && response.statusCode === 200) {

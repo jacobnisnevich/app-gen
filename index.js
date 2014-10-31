4b9234ac12f788a7309920bd16d87082818a4769
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-30 19:44:30 -0700

    major style changes

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
+var request = require('request');
 var express = require('express')
 var app = express();
-var request     = require('request');
 
-var apiKey      = '54f83928fbeec5b60aa70187744ca1ed';
+app.set('view engine', 'html');
+app.enable('view cache');
+app.engine('html', require('hogan-express'));
+
+var apiKey = '54f83928fbeec5b60aa70187744ca1ed';
 var category = 'game';
-
 var url = 'http://api.playstoreapi.com/v1.1/top/apps/' + category + '?key=' + apiKey;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
+app.use("/styles", express.static(__dirname + '/styles'));
 
 request({ url: url, json: true }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
@@ -35,6 +21,10 @@ request({ url: url, json: true }, function (error, response, body) {
 	  	}
     }
 });
+
+app.get('/', function(req, res) {
+  res.render('index.html')
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
